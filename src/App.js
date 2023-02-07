c21f8501b01cd0e9fc6e6a12483b0286596a7b07
nathanlao <gla93@sfu.ca>
2023-02-06 21:30:25 -0800

    Added navbar component and added nav image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import Navbar from './components/Navbar'
 
 export default function App() {
-  return (
-    <div>
-        <h1>hello world from React</h1>
-    </div>
-  )
+    return (
+      <div className='app'>
+        <div className='navbar'>
+          <Navbar />
+        </div>
+        <div className='main'>
+
+        </div>
+        <div className='footer'>
+
+        </div>
+      </div>
+    )
 }
