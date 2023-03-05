50f4c9e4f5cc5e4c21fc968d7a3665bd8baf8939
nathanlao <gla93@sfu.ca>
2023-03-04 21:51:05 -0800

    import antd.css

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import { Typography, Space } from 'antd'
 
+import Layout from './components/Layout.jsx'
+
 import Home from './pages/Home'
 import Cryptocurrencies from './pages/Cryptocurrencies'
 import CryptoDetails from './pages/CryptoDetails.jsx'
 import News from './pages/News.jsx'
 
-import Layout from './components/Layout.jsx'
+import "./App.css"
 
 export default function App() {
     return (
         <div className='app'>
           <div className='main'>
-                <div className='routes'>
-                    {/* Planning components to have: */}
-                    {/* Nested routes to have UI (navbar) shared */}
-                    <Routes>
-                      <Route path='/' element={<Layout />}>
-                        <Route index element={<Home />} />
-                        {/* Relative path */}
-                        <Route path='cryptocurrencies' element={<Cryptocurrencies />} />
-                        <Route path='crypto/:id' element={<CryptoDetails />} />
-                        <Route path='news' element={<News />} />
-                      </Route>
-                    </Routes>
-                </div>
+            <div className='routes'>
+                {/* Planning components to have: */}
+                {/* Nested routes to have UI (navbar) shared */}
+                <Routes>
+                  <Route path='/' element={<Layout />}>
+                    <Route index element={<Home />} />
+                    {/* Relative path */}
+                    <Route path='cryptocurrencies' element={<Cryptocurrencies />} />
+                    <Route path='crypto/:id' element={<CryptoDetails />} />
+                    <Route path='news' element={<News />} />
+                  </Route>
+                </Routes>
+            </div>
           </div>
           <div className='footer'>
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
+import { Layout } from "antd";
 
 export default function Layouts() {
     return (
-        <>
+        <Layout>
             <Navbar />
             <Outlet />
-        </>
+        </Layout>
     )
 }
