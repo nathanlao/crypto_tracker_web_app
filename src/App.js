23ac3f7dfd9eaabeb570d1e6d9338607bb40c88f
nathanlao <gla93@sfu.ca>
2023-03-04 20:32:38 -0800

    component restructured

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 export default function App() {
-    return (
-      <div className='app'>
-          <Navbar />
-        <div className='main'>
+  return (
+    <div className='app'>
+      <Navbar />
+      <div className='main'>
 
-        </div>
-        <div className='footer'>
-
-        </div>
       </div>
-    )
+      <div className='footer'>
+
+      </div>
+    </div>
+  )
 }
