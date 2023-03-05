bb7d762da3213083c2f288e141895e6a07a25e02
nathanlao <gla93@sfu.ca>
2023-03-04 21:10:01 -0800

    Set up nested routes for planning components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
-import { Layout, Typography, Space } from 'antd'
-import Navbar from './components/Navbar/Navbar.jsx'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Typography, Space } from 'antd'
+
+import Home from './components/Home'
+import Cryptocurrencies from './components/Cryptocurrencies'
+import CryptoDetails from './components/CryptoDetails.jsx'
+import News from './components/News.jsx'
+
+import Layout from './components/Layout.jsx'
 
 export default function App() {
-  return (
-    <div className='app'>
-      <Navbar />
-      <div className='main'>
+    return (
+        <div className='app'>
+          <div className='main'>
+                <div className='routes'>
+                    {/* Planning components to have: */}
+                    {/* Nested routes to have UI (navbar) shared */}
+                    <Routes>
+                      <Route path='/' element={<Layout />}>
+                        <Route index element={<Home />} />
+                        {/* Relative path */}
+                        <Route path='cryptocurrencies' element={<Cryptocurrencies />} />
+                        <Route path='crypto/:id' element={<CryptoDetails />} />
+                        <Route path='news' element={<News />} />
+                      </Route>
+                    </Routes>
+                </div>
+          </div>
+          <div className='footer'>
 
-      </div>
-      <div className='footer'>
-
-      </div>
-    </div>
-  )
+          </div>
+        </div>
+    )
 }
