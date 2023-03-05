55ef0dba70ceae5e5e3c1db5fb256b5cb7ca1a89
nathanlao <gla93@sfu.ca>
2023-03-04 22:18:55 -0800

    Added footer to layout (shared UI)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
 import { Layout } from "antd";
+
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 export default function Layouts() {
     return (
         <Layout>
             <Navbar />
             <Outlet />
+            <Footer />
         </Layout>
     )
 }
