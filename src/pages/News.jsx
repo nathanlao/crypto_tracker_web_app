6ccaffaa808e91758ae00c3923fe1d138ae88bbb
nathanlao <gla93@sfu.ca>
2023-03-11 18:52:14 -0800

    Using Axios to fetch data from Bing new search api

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function News() {
+
+    const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news',
+        params: {
+            safeSearch: 'Off', 
+            textFormat: 'Raw',
+            freshness: 'Day',
+            limit: '10',
+        },
+        headers: {
+            'X-BingApis-SDK': 'true',
+            'X-RapidAPI-Key': '2276dec5d9msha4514d2a5f75692p1e5181jsnda7948426efb',
+            'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+        }
+    };
+
+    useEffect(() => {
+        axios.request(options)
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <h1>News page here</h1>
     )
